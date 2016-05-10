8e0144920fc5fceaae7f5da73f7dc245468d5eb6
Laura <l4depende@gmail.com>
2016-05-10 22:10:08 +0100

    Base

---
+++ AnalizadorLexicoManual/LexicalAnalizerForÑ/LexicalAnalizerForÑ/keywords.h
@@ -0,0 +1,14 @@
+//
+//   STRwords.h
+//  LexicalAnalizerForÑ
+//
+//  Created by Laura del Pino Díaz on 2/4/16.
+//  Copyright © 2016 Laura del Pino Díaz. All rights reserved.
+//
+
+#ifndef  STRwords_h
+#define  STRwords_h
+
+std::string STRWORDSCONCAT = "sifincasofuncnuloparasinofinaliniciosalidaespaciodevuelvemientrasREALVECTORLETRAleeescribeglobal";
+
+#endif /*  STRwords_h */
